b446ff02528e4868bfb6c700d2632fa63fa78bd1
lapshaffer <lapshaffer@gmail.com>
2021-06-30 16:10:58 -0400

    added sections for timeblocks, thinking about having an object hold all of the events that will exist in local storage, but may scrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
     // Each time block needs to have an hour assigned to it
     // Based on the hour assigned and the current hour in real-time, the block must change color
     // The blocks must be able to take an input and have that input saved to local storage
-        // Do I want local storage to empty at midnight so each day has a clean slate?
+// Extra: Do I want local storage to empty at midnight so each day has a clean slate?
 
 
 // This is the code for the live clock
@@ -13,3 +13,22 @@ function renderClock() {
 };
 setInterval(renderClock, 1000);
 
+// Setting a variable for the current hour
+var currentHour = moment().format("HH");
+console.log(currentHour);
+
+// Maybe I need an object holding each hour?
+// And that's where we can hold the input values that are stored in local storage?
+var blockTimes = {
+    hourEight : "",
+    hourNine : "",
+    hourTen : "",
+    hourEleven : "",
+    hourTwelve : "",
+    hourThirteen : "",
+    hourFourteen : "",
+    hourFifteen : "",
+    hourSixteen : "",
+    hourSeventeen : "",
+    hourEighteen : "",
+}
