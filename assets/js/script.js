4bb947a42588c587bc8372f36dbd9df8bfcf63d8
lapshaffer <lapshaffer@gmail.com>
2021-07-08 18:17:35 -0400

    Received help from TA Andrew to rework main functions and they work now!

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@
         // If block asking if current hour is equal to the set hour to determine block color
     // The blocks must be able to take an input and have that input saved to local storage
 // Extra: Do I want local storage to empty at midnight so each day has a clean slate?
+    // 07/08/2021- Just got the JS working, so I may go back later and have the local storage wipe at midnight!
 
 
 // This is the code for the live clock
@@ -21,52 +22,23 @@ var currentHour = moment().hour();
 // This console log confirmed that hours are stored in a 24 hour format rather than 12, so each timeblock will need an assigned hour from hour 8 to hour 18
 console.log(currentHour);
 
-// Not currently working 
-// Getting an error that saveBtn.addEventListener isn't a function
 // adding a variable to represent the save button
 var saveBtn = document.querySelectorAll(".saveBtn");
-// adding event listener to save button to save textArea to local storage
-function saveButton() { saveBtn.addEventListener("click", function() {
-    // creating a variable to hold the event description from the textArea
-    // This works because the textArea is the sibling of the saveBtn, and has the class of .description
-    var description = $(this).siblings(".description").val();
-    // creating a variable to hold the time of the event
-    // This works because the timeblock is the parent of the saveBtn, and it saves the id of each block, which is the time
-    var time = $(this).parent().attr("id");
-    // saving the time and event description to local storage
-    localStorage.setItem(time, description);
-})};
+// for each loop to cycle through each save button
+saveBtn.forEach(function(node) {
+    // adding event listener to save button to save textArea to local storage
+    node.addEventListener("click", function() {
+        // creating a variable to hold the event description from the textArea
+        // This works because the textArea is the sibling of the saveBtn, and has the class of .description
+        var description = $(this).siblings(".description").val();
+        // creating a variable to hold the time of the event
+        // This works because the timeblock is the parent of the saveBtn, and it saves the id of each block, which is the time
+        var time = $(this).parent().attr("id");
+        // saving the time and event description to local storage
+        localStorage.setItem(time, description);
 
-saveButton();
-
-// Not currently working
-function colorChange() {
-    // for each timeblock, run this function
-    $(".time-block").each(function () {
-        // read the id of each timeblock remove the "hour" and return the number to the variable
-        // right now, this isn't working for times past 9am, probably because the other numbers are double digits?
-        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-        console.log(blockTime);
-        if (blockTime < currentHour) {
-            $(this).addClass("past");
-            $(this).removeClass("present");
-            $(this).removeClass("future");
-        }
-        else if (blockTime === currentHour) {
-            $(this).removeClass("past");
-            $(this).addClass("present");
-            $(this).removeClass("future");
-        }
-        else {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            $(this).addClass("future");
-
-        }
     })
-};
-
-colorChange(); 
+})
 
 // Writing the local storage to the textArea if there is a description in local storage 
 $("#hour8 .description").val(localStorage.getItem("hour8"));
@@ -80,3 +52,32 @@ $("#hour15 .description").val(localStorage.getItem("hour15"));
 $("#hour16 .description").val(localStorage.getItem("hour16"));
 $("#hour17 .description").val(localStorage.getItem("hour17"));
 $("#hour18 .description").val(localStorage.getItem("hour18"));
+
+function colorChange() {
+    // for each timeblock, run this function
+    $(".time-block").each(function () {
+        // read the id of each timeblock remove the "hour" and return the number to the variable
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+        // If the number of the timeblock is before the current hour
+        if (blockTime < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+        }
+        // if the number of the timeblock is during the current hour
+        else if (blockTime === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        // if the number of the timeblock is after the current hour
+        else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+
+        }
+    })
+};
+
+colorChange(); 
